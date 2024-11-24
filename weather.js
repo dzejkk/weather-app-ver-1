1b9fc562dd6c47c3a763ebcb8861727fd986416c
Jakub Kontra <kontrajakub2@gmail.com>
2024-11-24 13:43:48 +0000

    progres,  Adding  weather API,

---
+++ weather.js
@@ -0,0 +1,5 @@
+
+//  https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature,precipitation_probability,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&timeformat=unixtime&timezone=America%2FChicago
+
+
+export function  getWeather(lat, lon, timezone) {}
