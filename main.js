bb92958fc5dd468833cd82784787da7b568beb61
Jakub Kontra <kontrajakub2@gmail.com>
2024-11-24 16:30:10 +0000

    added, new API key, but it not frsh, must remake app with, acctual API data

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-import './style.css'
-import  {getWeather} from "./weather.js";
+import "./style.css";
+import { getWeather } from "./weather.js";
+
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
+  .then(renderWeather)
+  .catch((e) => {
+    console.error(e);
+    alert("Error getting weather data");
+  });
+
+function renderWeather({ current, daily, hourly }) {
+  renderCurrentWeather(current);
+  //renderDailyWeather(daily);
+  //renderHourlyWeather(hourly);
+
+  document.body.classList.remove("blurred");
+
+}
 
 
-getWeather(10,10,Intl.DateTimeFormat().resolvedOptions().timeZone).then( data => {
-    console.log(data);
-})
-
-
+function renderCurrentWeather(current) {
+    document.querySelector ( "[data-current-temp]").textContent = current.currentTemp;
+}
