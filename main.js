3d4478dccd3cd25ca2b2ea89c7e1c038d3d6d714
Jakub Kontra <kontrajakub2@gmail.com>
2024-11-24 14:19:53 +0000

    progress, geetting data form API, and creating variables

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 import './style.css'
+import  {getWeather} from "./weather.js";
+
+
+getWeather(10,10,Intl.DateTimeFormat().resolvedOptions().timeZone).then( result => {
+    console.log(result.data);
+})
 
 
--- weather.js
+++ weather.js
@@ -1,5 +1,57 @@
+import Axios from "axios";
+import axios from "axios";
 
-//  https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature,precipitation_probability,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&timeformat=unixtime&timezone=America%2FChicago
+/*
+ https://api.open-meteo.com/v1/forecast?&hourly=temperature_2m,apparent_temperature,precipitation_probability,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&timeformat=unixtime
+
+ */
 
 
-export function  getWeather(lat, lon, timezone) {}
+export function getWeather(lat, lon, timezone) {
+   return axios.get("https://api.open-meteo.com/v1/forecast?&hourly=temperature_2m,apparent_temperature,precipitation_probability,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&timeformat=unixtime", {
+    params: {
+      latitude: lat,
+      longitude: lon,
+      timezone: timezone,
+    },
+  }
+  ).then(({data}) => {
+      return  {
+          current: parseCurrentWeather(data),
+          daily: parseDailyWeather(data),
+          hourly: parseHourlyWeather(data)
+
+      }
+   });
+}
+
+
+function parseCurrentWeather({current_weather, daily}) {
+
+    const {
+        temperature: currentTemp,
+        windSpeed: windSpeed,
+        weathercode: iconCode}
+        = current_weather;
+
+    const {
+        temperature_2m_max: [maxTemp],
+        temperature_2m_max: [maxTemp],
+        temperature_2m_max: [maxTemp],
+        temperature_2m_max: [maxTemp],
+        temperature_2m_max: [maxTemp]
+
+    } = daily;
+
+    return {
+        currentTemp,
+        highTemp: ,
+        lowTemp: ,
+        highFeelsLike: ,
+        lowFeelsLike: ,
+        windSpeed,
+        iconCode
+
+    }
+}
+
