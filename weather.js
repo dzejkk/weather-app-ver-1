e6db19b14d7141dc5c2b0ff956ff962b05d35579
Jakub Kontra <kontrajakub2@gmail.com>
2025-05-03 11:40:22 +0000

    nachystane data

--- weather.js
+++ weather.js
@@ -14,12 +14,12 @@ export function getWeather(lat, lon, timezone) {
     })
     .then(({ data }) => {
       // shorhand for   then.response,  const data = response
-      console.log(data);
+      //console.log(data);
 
       return {
         currentWeather: parseCurrentWeather(data), //vratenie  parsovanych premennych
         dailyWeaher: parseDailyWeather(data),
-        //hourlyWeather: parseHourlyWeather(data),
+        hourlyWeather: parseHourlyWeather(data),
       };
     });
 }
@@ -55,13 +55,26 @@ function parseCurrentWeather({ current, daily }) {
 function parseDailyWeather({ daily }) {
   // pozor teraz musis loopnut cez object poli
 
-  return daily.map((time, index) => {
+  return daily.time.map((time, index) => {
     return {
-      timestamp: time * 1000, // JS want miliseconds
-      iconCode: daily.weathercode[index],
+      date: time,
+      iconCode: daily.weather_code[index],
       maxTemp: daily.temperature_2m_max[index],
     };
   });
 }
 
-//function parseHourlyWeather({ hourly, current_weather }) {}
+function parseHourlyWeather({ hourly, current }) {
+  return hourly.time
+    .map((time, index) => {
+      return {
+        date: time,
+        dayStatus: hourly.is_day[index],
+        iconCode: hourly.weather_code[index],
+        temp: hourly.temperature_2m[index],
+        humidity: hourly.relative_humidity_2m[index],
+        windSpeed: hourly.wind_speed_10m[index],
+      };
+    })
+    .filter(({ date }) => new Date(date) >= new Date(current.time));
+}
